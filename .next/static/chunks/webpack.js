bbe3f95b30413b08c0364452b1eff07fe4fe06d8
Adrian Roldan <adrianaer7@hotmail.com>
2022-04-01 20:18:00 -0300

    ASd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c55345ea2fee0d14"; }
+/******/ 		__webpack_require__.h = function() { return "4767f09557776ce3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
