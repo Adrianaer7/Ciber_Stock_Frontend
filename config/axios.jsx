4285a79bb4f385ff2519942adcfd2eabdb486562
Adrianaer7 <adrianaer7@hotmail.com>
2022-04-15 14:13:09 -0300

    Es

--- config/axios.jsx
+++ config/axios.jsx
@@ -3,5 +3,5 @@ import axios from "axios"
 const clienteAxios = axios.create({
     baseURL: process.env.backendURL
 })
-
+console.log(process.env.backendURL)
 export default clienteAxios;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    backendURL: "https://stock-servidor.herokuapp.com"
+    backendURL: "https://stock-servidor.herokuapp.com",
+    MONGO_DB: "mongodb+srv://admin:admin@cluster0.nrmxp.mongodb.net/ciberinfotel"
   }
 }
