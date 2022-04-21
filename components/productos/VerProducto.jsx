15b83b503efd8af5016762d1324d2062d6e8a494
Adrianaer7 <adrianaer7@hotmail.com>
2022-04-21 15:42:14 -0300

    ASD

--- components/productos/VerProducto.jsx
+++ components/productos/VerProducto.jsx
@@ -101,7 +101,7 @@ const VerProducto = ({producto}) => {
 
                         <tr className=" dark:bg-gray-900 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-900 even:dark:bg-gray-700  justify-between grid  grid-cols-1 lg:grid-cols-2">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap text-left">CÓDIGO DE BARRAS</th>
-                            <td className="px-6 py-4 text-left">{barras}</td>
+                            <td className="px-6 py-4 text-left">{barras ? barras : "-"}</td>
                         </tr>
                         
                         <tr className=" dark:bg-gray-900 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-900 even:dark:bg-gray-700  justify-between grid  grid-cols-1 lg:grid-cols-2">
